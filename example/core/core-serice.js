0e3e61c8880fe749eacbd861ef65b6157a240df5
Azmi SAHIN <azmisahin@outlook.com>
2024-08-03 01:36:12 +0300

    örnek core servisi

---
+++ example/core/core-serice.js
@@ -0,0 +1,24 @@
+const { Kafka } = require('kafkajs');
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['kafka1:9092', 'kafka2:9092']
+});
+
+const consumer = kafka.consumer({ groupId: 'test-group' });
+
+const run = async () => {
+  await consumer.connect();
+  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log({
+        value: message.value.toString(),
+      });
+      // İşlem mantığı burada yer alır.
+    },
+  });
+}
+
+run().catch(console.error);
